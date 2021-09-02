5737269ad75677a03e434cf0bfb89d3f5103db8b
unknown <nasrullah_hussaini@yahoo.com>
2021-09-02 00:18:07 -0400

    create a route using express.Route middleware

--- server/index.js
+++ server/index.js
@@ -4,15 +4,15 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.get('/', (req,res)=>{
-  res.send('hi');
-})
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.use(router);
+
+
+
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
